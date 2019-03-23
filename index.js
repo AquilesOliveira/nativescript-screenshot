67216970bb01a7ceb3c27d443750eb2f14f4f5ce
Aquiles Oliveira <aquilesoliveiradesouza@gmail.com>
2019-03-22 21:15:22 -0300

    Recent iOS issue resolution
    Simple modification...

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var imageSource = require("image-source");
 
 exports.getImage = function(view) {
     if (view.ios) {
-        UIGraphicsBeginImageContextWithOptions(view.ios.frame.size, false, 0);
-        view.ios.drawViewHierarchyInRectAfterScreenUpdates(CGRectMake(0, 0, view.ios.frame.size.width, view.ios.frame.size.height), true);
-        var imageFromCurrentImageContext = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return imageSource.fromData(UIImagePNGRepresentation(imageFromCurrentImageContext));
-        
+      UIGraphicsBeginImageContextWithOptions(view.ios.frame.size, false, 0);
+      view.ios.drawViewHierarchyInRectAfterScreenUpdates(CGRectMake(0, 0, view.ios.frame.size.width, view.ios.frame.size.height), true);
+      let imageFromCurrentImageContext = UIGraphicsGetImageFromCurrentImageContext();
+      const source: ImageSource = <ImageSource> fromData(UIImagePNGRepresentation(imageFromCurrentImageContext));
+      return source;
     } else if (view.android) {
         view.android.setDrawingCacheEnabled(true);
         var bmp = android.graphics.Bitmap.createBitmap(view.android.getDrawingCache());
